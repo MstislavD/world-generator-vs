aa7c0d7be72742863801ac9d6ef162ada4a12fb7
Mstislav Dobrotvorskiy <mstislavd@gmail.com>
2025-10-16 11:55:58 +0300

    update GridLayerGenerator

--- Topology/GridLayerGenerator.cs
+++ Topology/GridLayerGenerator.cs
@@ -9,8 +9,9 @@ namespace Topology
 {
     public interface ILayerCell<TCell>
     {
-        List<TCell>? Children { get; }
         TCell? Parent { get; set; }
+        IEnumerable<TCell>? Children { get; }
+        int ChildrenCount { get; }
         void AddChild(TCell cell);
     }
 
@@ -18,8 +19,8 @@ namespace Topology
     {
         List<TCell>? _children;
         public TCell? Parent { get; set; }
-        public List<TCell>? Children => _children;
-
+        public IEnumerable<TCell>? Children => _children;
+        public int ChildrenCount => _children == null ? 0 : _children.Count;
         public void AddChild(TCell cell)
         {
             _children = _children ?? new List<TCell>();
@@ -35,41 +36,66 @@ namespace Topology
     public class GridLayerGenerator
     {
         /// <summary>
-        /// The exact children of parent cells of the first and last row also lie in these rows
+        /// Generates child grid from a grid.
         /// </summary>
-        /// <param name="parentGrid"></param>
+        /// <param name="parentGrid">Parent grid.</param>
         /// <param name="factory"></param>
         /// <param name="random"></param>
+        /// <param name="wideBorders">False if exact children of parent cells of the first and last row also lie in these rows.</param>
         /// <returns></returns>
-        static public TGrid CreateLayerGrid<TGrid, TCell>(TGrid parentGrid, IFactoryHexGrid<TGrid> factory, RandomExt random)
+        static public TGrid CreateLayerGrid<TGrid, TCell>(TGrid parentGrid, IFactoryHexGrid<TGrid> factory, RandomExt random, bool wideBorders = false)
             where TGrid : IGrid<TCell>
             where TCell : ILayerCell<TCell>, INeighbors<TCell>
         {
-            TGrid childGrid = factory.CreateGrid(parentGrid.Columns * 2, parentGrid.Rows * 2 + 1);
+            int dh = wideBorders ? 1 : -1;
+            TGrid childGrid = factory.CreateGrid(parentGrid.Columns * 2, parentGrid.Rows * 2 + dh);
 
+            int dy = wideBorders ? 1 : 0;
             for (int x = 0; x < parentGrid.Columns; x++)
             {
                 for (int y = 0; y < parentGrid.Rows; y++)
                 {
                     TCell parentCell = parentGrid.GetCell(x, y);
                     int childX = x * 2 + y % 2;
-                    int childY = y * 2 + 1;
+                    int childY = y * 2 + dy;
                     TCell childCell = childGrid.GetCell(childX, childY);
                     childCell.Parent = parentCell;
                     parentCell.AddChild(childCell);
                 }
             }
 
-            Dictionary<TCell, TCell?> parentByCell = new();
+            int sizeVariance = 0; // dummy
             foreach (TCell cell in childGrid.Cells.Where(c => c.Parent == null))
             {
-                List<TCell?> parentCandidates = cell.Neighbors.Select(n => n.Parent).Where(p => p != null).ToList();
-                TCell? parent = random.NextItem(parentCandidates);
-                parentByCell[cell] = parent;
-            }
-            foreach (var item in parentByCell)
-            {
-                item.Key.Parent = item.Value;
+                List<TCell> parentCandidates = cell.Neighbors.Select(n => n.Parent).Where(p => p != null).Distinct().ToList();
+
+                TCell? parent = default;
+                if (parentCandidates.Count > 1)
+                {                    
+                    if (sizeVariance > 0) // dummy check
+                    {
+                        int sizeDelta = parentCandidates[0].ChildrenCount - parentCandidates[1].ChildrenCount;
+                        if (sizeDelta > sizeVariance)
+                        {
+                            parent = parentCandidates[1];
+                        }
+                        else if (sizeDelta < -sizeVariance)
+                        {
+                            parent = parentCandidates[0];
+                        }
+                        else
+                        {
+                            parent = random.NextItem(parentCandidates);
+                        }
+                    }
+                }
+                else
+                {
+                    parent = parentCandidates[0];
+                }
+
+                cell.Parent = parent ?? throw new Exception();
+                parent.AddChild(cell);
             }
 
             return childGrid;
